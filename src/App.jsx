b6ad5f31a9b247ff93ee24f014dbc482ac2ea41d
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-11-22 13:59:49 +0330

    added react files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import PhysicalProductPage from "./components/PhysicalProductPage";
+function App() {
+  return (
+    <div className="App">
+      <PhysicalProductPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Physical-Product-Page.jsx
@@ -0,0 +1,10 @@
+// import Footer from "./Footer";
+function PhysicalProductPage() {
+  return (
+    <div className="PhysicalProductPage">
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default PhysicalProductPage;

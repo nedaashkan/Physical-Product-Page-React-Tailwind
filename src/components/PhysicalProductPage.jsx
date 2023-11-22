47dcb18f0c7a8dd5c4a1e646de7bc5f6aee36586
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-11-22 14:29:08 +0330

    added Tailwind css

--- src/components/PhysicalProductPage.jsx
+++ src/components/PhysicalProductPage.jsx
@@ -1,7 +1,7 @@
 function PhysicalProductPage() {
   return (
     <div className="PhysicalProductPage">
-      <h1>hi</h1>
+      <h1 className="text-3xl font-bold underline">hi</h1>
     </div>
   );
 }

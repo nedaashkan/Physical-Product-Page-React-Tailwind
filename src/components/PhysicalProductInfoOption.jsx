3bb18d9aaa5ee6b3a9afab5701e8945824237df6
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-11-24 00:52:24 +0330

    Update PhysicalProductInfoOption.jsx

--- src/components/PhysicalProductInfoOption.jsx
+++ src/components/PhysicalProductInfoOption.jsx
@@ -1,15 +1,16 @@
 function PhysicalProductInfoOption() {
   return (
-    <div classNameName="PhysicalProductInfoOption w-full lg:w-2/6 md:w-2/6">
+    <div classNameName="PhysicalProductInfoOption w-full lg:max-w-2/6 md:max-w-2/6">
       <div className="flex flex-col mx-2 p-1 md:p-3 lg:p-3 md:mx-0 lg:mx-0">
+        {/* product info */}
         <div className="flex flex-col">
-          <h2 className="text-[0.9rem] md:text-[0.9rem] lg:text-[1rem] text-center text-[#CFCFCF] mb-3 md:mb-5 lg:mb-6 ">
+          <h2 className="text-[0.9rem] md:text-[0.8rem] lg:text-[0.9rem] text-center text-[#CFCFCF] mb-3 md:mb-5 lg:mb-6 ">
             فیزیکال . رسته . نام شرکت سازنده و محصول
           </h2>
-          <h1 className="text-[1.1rem] md:text-[1.2rem] lg:text-[1.3rem]  text-center md:text-start lg:text-start text-[#00d5ff] mb-2 md:mb-2 lg:mb-2">
+          <h1 className="text-[1.1rem] md:text-[1.1rem] lg:text-[1.3rem]  text-center md:text-start lg:text-start text-[#00d5ff] mb-2 md:mb-2 lg:mb-2">
             Product Name Product Name
           </h1>
-          <p className="text-[0.7rem] md:text-[0.9rem] lg:text-[1rem] text-center md:text-start lg:text-start  text-[#CFCFCF]  mb-1 md:mb-1 lg:mb-1">
+          <p className="text-[0.7rem] md:text-[0.8rem] lg:text-[0.9rem] text-center md:text-start lg:text-start  text-[#CFCFCF]  mb-1 md:mb-1 lg:mb-1">
             Product Description Product Description
             <br className="" />
             Product Description
@@ -22,40 +23,50 @@ function PhysicalProductInfoOption() {
           </h3>
           <li
             dir="rtl"
-            className="text-[0.5rem] md:text-[0.9rem] lg:text-[1rem] text-[#3A3A3A] text-center md:text-start lg:text-start mb-2 md:mb-1"
+            className="text-[0.5rem] md:text-[0.9rem] lg:text-[0.9rem] text-[#3A3A3A] text-center md:text-start lg:text-start mb-2 md:mb-1"
           >
-            <span className="text-[0.7rem] md:text-[0.6rem] lg:text-[0.7rem]  text-[#CFCFCF]">
+            <span className="text-[0.7rem] md:text-[0.5rem] lg:text-[0.7rem]  text-[#CFCFCF]">
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم لورم ایپسوم
             </span>
           </li>
           <li
             dir="rtl"
-            className="text-[0.5rem] md:text-[0.9rem] lg:text-[1rem] text-[#3A3A3A] text-center md:text-start lg:text-start mb-2 md:mb-1"
+            className="text-[0.5rem] md:text-[0.9rem] lg:text-[0.9rem] text-[#3A3A3A] text-center md:text-start lg:text-start mb-2 md:mb-1"
           >
-            <span className="text-[0.7rem] md:text-[0.6rem] lg:text-[0.7rem]  text-[#CFCFCF]">
+            <span className="text-[0.7rem] md:text-[0.5rem] lg:text-[0.7rem]  text-[#CFCFCF]">
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم لورم ایپسوم
             </span>
           </li>
           <li
             dir="rtl"
-            className="text-[0.5rem] md:text-[0.9rem] lg:text-[1rem] text-[#3A3A3A] text-center md:text-start lg:text-start mb-2 md:mb-1"
+            className="text-[0.5rem] md:text-[0.9rem] lg:text-[0.9rem] text-[#3A3A3A] text-center md:text-start lg:text-start mb-2 md:mb-1"
           >
-            <span className="text-[0.7rem] md:text-[0.6rem] lg:text-[0.7rem]  text-[#CFCFCF]">
+            <span className="text-[0.7rem] md:text-[0.5rem] lg:text-[0.7rem]  text-[#CFCFCF]">
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم لورم ایپسوم
             </span>
           </li>
         </div>
-
         <br />
-
+        <br />
+        <br />
+        <br />
+        <br />
+        {/* more product */}
         <div className="flex flex-col">
-          <h2 className="">پیشنهادات دیگر</h2>
-          <div className="flex flex-row">
-            <img src="" alt="" srcset="" />
-            <div className="flex flex-row">
-              <p className="">Product Name Product Name</p>
-              <h3 className="flex flex-row-reverse justify-end">
-                100,000,000 <span className="">تومان</span>
+          <h2 className="text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] text-center md:text-start lg:text-start  text-[#CFCFCF]  mb-1 md:mb-1 lg:mb-6">
+            پیشنهادات دیگر
+          </h2>
+          <div className="flex flex-row items-center">
+            <img src="/src/assets/ProductImg/2.png" alt="" className="w-fit" />
+            <div className="flex flex-col">
+              <p className="text-[0.8rem] md:text-[0.9rem] lg:text-[1rem]  text-[#CFCFCF]">
+                Product Name Product Name
+              </p>
+              <h3 className=" text-[0.8rem] md:text-[0.9rem] lg:text-[1rem]  text-[#CFCFCF]  flex flex-row-reverse justify-center lg:justify-end md:justify-end">
+                100,000,000
+                <span className="text-[0.7rem] md:text-[0.8rem] lg:text-[0.6rem] text-[#CFCFCF] flex justify-center items-center p-1">
+                  تومان
+                </span>
               </h3>
             </div>
           </div>
